8b1174367951bcbdb21c842ddb3d680c6ce7591d
Bogdan Shkaran <bogdan.shkaran@gmail.com>
2022-02-08 15:47:21 +0200

    handle data from inputs in emp-add-form

--- src/components/employees-add-form/employees-add-form.jsx
+++ src/components/employees-add-form/employees-add-form.jsx
@@ -1,23 +1,49 @@
+import { Component } from 'react';
+
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Add a new employee</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="His name" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="Salary ($)" />
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        };
+    }
 
-                <button type="submit"
-                        className="btn btn-outline-light">Add</button>
-            </form>
-        </div>
-    );
+    onValueChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    render() {
+        const { name, salary } = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Add a new employee</h3>
+                <form
+                    className="add-form d-flex">
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="His name"
+                        name="name"
+                        value={name} 
+                        onClick={this.onValueChange}/>
+                    <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="Salary ($)"
+                        name="salary"
+                        value={salary} 
+                        onClick={this.onValueChange}/>
+    
+                    <button type="submit"
+                            className="btn btn-outline-light">Add</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default EmployeesAddForm;
